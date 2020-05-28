2d44bc560127b570acd50f488fec6ad2b36baf66
ruymaster <mecury1984@outlook.com>
2020-05-28 21:17:47 +0800

    add build cpp-ipfs-http-client

--- build-windows.sh
+++ build-windows.sh
@@ -11,10 +11,8 @@ echo "" >build-windows.log
 #
 # Build the restclient-cpp for OSX
 #
-
-echo Building Windows version of restclient-cpp
-
-echo Building Windows version of gtest and curl library for restclient-cpp
+echo "*****************************************"
+echo Building Windows version of gtest and curl library
 yes | cp -rf ./build-patch/*  ./restclient-cpp/vendor/gtest-1.7.0/cmake/
 
 cd restclient-cpp
@@ -24,26 +22,35 @@ try mkdir -p build/Windows/
 try cd build/Windows
 cmake ../.. -G "Visual Studio 15 2017 Win64"
 cmake --build . --config Release
-cd ..
-cd ..
-cd ../../..
-cd curl-android-ios
+
+cd $rootdir/curl-android-ios
 cd curl
 try mkdir -p build/Windows/
 try cd build/Windows
+try rm CMakeCache.txt
 cmake ../.. -G "Visual Studio 15 2017 Win64" -DBUILD_TESTING=OFF -DCMAKE_USE_OPENSSL=ON -DCURL_STATICLIB=ON
 cmake --build . --config Release
-cd ..
-cd ..
-cd ..
-cd ../restclient-cpp
+
+echo "*****************************************"
+echo "Building Windows version of restclient-cpp"
+cd $rootdir/restclient-cpp
 try mkdir -p build/Windows/
 try cd build/Windows
+try rm CMakeCache.txt
 cmake ../.. -G "Visual Studio 15 2017 Win64"  -DCURL_LIBRARY=../../../curl-android-ios/curl/build/lib/Release/libcurl.lib    -DCURL_INCLUDE_DIR=../../../curl-android-ios/curl/include -DBUILD_SHARED_LIBS=NO
 cmake --build . --config Release
-cd ..
-cd ..
-cd ..
+echo "*****************************************"
+echo "Building Windows version of cpp-ipfs-http-client"
+cd $rootdir/cpp-ipfs-http-client
+try mkdir -p build/Windows/
+try cd build/Windows
+currentDir=$(pwd)
+$CacheFile = $currentDir/CMakeCache.txt
+if [ -f "$CacheFile" ]; then    
+	try rm CMakeCache.txt
+fi
 
+cmake ../.. -G "Visual Studio 15 2017 Win64"  -DCURL_LIBRARY=../../../curl-android-ios/curl/build/Windows/lib/Release/libcurl.lib    -DCURL_INCLUDE_DIR=../../../curl-android-ios/curl/include -DBUILD_SHARED_LIBS=NO -DJSON_FOR_MODERN_CXX_INCLUDE_DIR=../../../json/include -DBUILD_TESTING=OFF
+cmake --build . --config Release
 #try cmake  -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../../Lib/Windows -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=../../Lib/Windows ../../
 
