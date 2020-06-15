e0779e072a1063fa10886a577d6e1f619a059012
YaoPin <mecury1984@outlook.com>
2020-06-15 23:27:07 +0800

    delete temp directory

--- build/temp/GSL/tmp/GSL-cache-Debug.cmake
+++
@@ -1,3 +0,0 @@
-
-set(CMAKE_INSTALL_PREFIX "/media/pik/Work10/Work/SuperGeniusWork/GeniusVenture/GeniusTokens/thirdparty/build/temp/GSL" CACHE PATH "Initial cache" FORCE)
-set(GSL_TEST "OFF" CACHE BOOL "Initial cache" FORCE)
