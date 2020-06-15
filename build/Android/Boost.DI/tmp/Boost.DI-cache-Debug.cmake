6e031e6edbc05250f2b15b6934b59eb44d12891a
YaoPin <mecury1984@outlook.com>
2020-06-15 23:55:44 +0800

    add gitignore

--- build/Android/Boost.DI/tmp/Boost.DI-cache-Debug.cmake
+++
@@ -1,4 +0,0 @@
-
-set(BOOST_DI_OPT_BUILD_TESTS "OFF" CACHE BOOL "Initial cache" FORCE)
-set(BOOST_DI_OPT_BUILD_EXAMPLES "OFF" CACHE BOOL "Initial cache" FORCE)
-set(CMAKE_INSTALL_PREFIX "/media/pik/Work10/Work/SuperGeniusWork/GeniusVenture/GeniusTokens/thirdparty/build/Android/Boost.DI" CACHE PATH "Initial cache" FORCE)
--- build/Android/tsl_hat_trie/tmp/tsl_hat_trie-cache-Debug.cmake
+++
@@ -1,2 +0,0 @@
-
-set(CMAKE_INSTALL_PREFIX "/media/pik/Work10/Work/SuperGeniusWork/GeniusVenture/GeniusTokens/thirdparty/build/Android/hat-trie" CACHE PATH "Initial cache" FORCE)
