b5e00f81de2d7270b4dcc3478f46ec23e0836faf
devcareer0 <fldevcareer0@gmail.com>
2021-07-08 19:06:19 +0200

    Updated Android build configuration

--- build/Android/Boost-build/build.sh
+++ build/Android/Boost-build/build.sh
@@ -5,7 +5,7 @@
 # also useful ndk details:
 # https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md
 
-SAVED_PATH=$PATH 
+SAVED_PATH=$PATH
 export PATH=$(pwd)/bin:$SAVED_PATH
 
 #----------------------------------------------------
@@ -30,17 +30,26 @@ ABI_NAMES=$2
 # ABI_NAME=arm64-v8a
 BOOST_DIR=$3
 
+# release | debug
+
+BUILD_VARIANT="release"
+if [ -n "$4" ]; then
+  if [ "$4" = "Debug" ] || [ "$4" = "debug" ]; then
+    BUILD_VARIANT="debug"
+  fi
+fi
+
 # only build these libs
 WITH_LIBRARIES="--with-thread --with-program_options --with-system --with-date_time --with-regex --with-chrono --with-atomic --with-random --with-filesystem  --with-log"
 
 
 LOG_FILE=${BUILD_DIR}/build.log
-#empty logFile 
-if [ -f "$LOG_FILE" ]  
-then 
+#empty logFile
+if [ -f "$LOG_FILE" ]
+then
     rm $LOG_FILE
-fi          
-    
+fi
+
 
 
 
@@ -51,7 +60,7 @@ toolset_for_abi_name() {
 
 
     local abi_name=$1
-    
+
     case "$abi_name" in
         arm64-v8a)      echo "arm64v8a"
         ;;
@@ -61,7 +70,7 @@ toolset_for_abi_name() {
         ;;
         x86_64)         echo "x8664"
         ;;
-        
+
     esac
 }
 #----------------------------------------------------------------------------------
@@ -69,7 +78,7 @@ toolset_for_abi_name() {
 clang_triple_for_abi_name() {
 
     local abi_name=$1
-    
+
     case "$abi_name" in
         arm64-v8a)      echo "aarch64-linux-android21"
         ;;
@@ -79,9 +88,9 @@ clang_triple_for_abi_name() {
         ;;
         x86_64)         echo "x86_64-linux-android21"
         ;;
-        
+
     esac
-    
+
 }
 #----------------------------------------------------------------------------------
 # map abi to {ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/*-ranlib etc
@@ -98,16 +107,16 @@ tool_triple_for_abi_name() {
         ;;
         x86_64)         echo "x86_64-linux-android"
         ;;
-        
+
     esac
-    
+
 }
 #----------------------------------------------------------------------------------
 abi_for_abi_name() {
 
 
     local abi_name=$1
-    
+
     case "$abi_name" in
         arm64-v8a)      echo "aapcs"
         ;;
@@ -117,15 +126,15 @@ abi_for_abi_name() {
         ;;
         x86_64)         echo "sysv"
         ;;
-        
+
     esac
-    
+
 }
 #----------------------------------------------------------------------------------
 arch_for_abi_name() {
 
     local abi_name=$1
-    
+
     case "$abi_name" in
         arm64-v8a)      echo "arm"
         ;;
@@ -135,16 +144,16 @@ arch_for_abi_name() {
         ;;
         x86_64)         echo "x86"
         ;;
-        
+
     esac
-    
+
 }
 
 #----------------------------------------------------------------------------------
 address_model_for_abi_name() {
 
     local abi_name=$1
-    
+
     case "$abi_name" in
         arm64-v8a)      echo "64"
         ;;
@@ -154,20 +163,20 @@ address_model_for_abi_name() {
         ;;
         x86_64)         echo "64"
         ;;
-        
+
     esac
-    
+
 }
 #----------------------------------------------
 compiler_flags_for_abi_name() {
 
     local abi_name=$1
-    
+
     COMMON_FLAGS="" #-fno-integrated-as -Wno-long-long"
     local ABI_FLAGS
     case "$abi_name" in
         armeabi-v7a)
-            ABI_FLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp" 
+            ABI_FLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
             ;;
         arm64-v8a|x86|x86_64)
             ;;
@@ -175,14 +184,14 @@ compiler_flags_for_abi_name() {
             echo "ERROR: Unknown ABI : $ABI" 1>&2
             exit 1
     esac
-    
+
     echo "$COMMON_FLAGS $ABI_FLAGS"
-}  
+}
 #----------------------------------------------
 linker_flags_for_abi_name() {
 
     local abi_name=$1
-    
+
     COMMON_FLAGS=""
     local ABI_FLAGS
     case "$abi_name" in
@@ -195,9 +204,9 @@ linker_flags_for_abi_name() {
             echo "ERROR: Unknown ABI : $ABI" 1>&2
             exit 1
     esac
-    
+
     echo "$COMMON_FLAGS $ABI_FLAGS"
-}   
+}
 #----------------------------------------------------------------------------------
 # write the ndk version to a header file for future reference and programmatic querying
 persist_ndk_version()
@@ -205,39 +214,39 @@ persist_ndk_version()
     # get the version string from the "Pkg.Revision" attribute in the $ANDROID_NDK_ROOT"/source.properties" file
     # and write this to a new header file (beside include/boost/version.hpp which documents the boost version)
     local source_properties=${ANDROID_NDK}/source.properties
-    
+
     local dir_path=${PREFIX_DIR}/include/boost
     mkdir --parents $dir_path
     local headerFile=${dir_path}/version_ndk.hpp
-    
-   
+
+
    local version=$(sed -En -e 's/^Pkg.Revision\s*=\s*([0-9a-f]+)/\1/p' $source_properties)
-    
-   
+
+
    echo "writing NDK version $version to $headerFile "
-    
+
    echo '#ifndef BOOST_VERSION_NDK_HPP'  > $headerFile
    echo '#define BOOST_VERSION_NDK_HPP' >> $headerFile
 
    echo -e '\n//The version of the NDK used to build boost' >>  $headerFile
    echo -e " #define BOOST_BUILT_WITH_NDK_VERSION  \"$version\" \\n" >>$headerFile
-   
+
    echo '#endif' >>$headerFile
-    
+
 
 }
 
 #----------------------------------------------------------------------------------
 fix_version_suffices() {
 
-# 1) remove files that are symbolic links 
+# 1) remove files that are symbolic links
 # 2)  remove version suffix on (remaining):
-#   a) file names and 
+#   a) file names and
 #   b) in the "soname" of the elf header
- 
+
 
     Re0="^libboost_(.*)\.so"
-    Re1="(.[[:digit:]]+){3}$" 
+    Re1="(.[[:digit:]]+){3}$"
     #Re1="(.[0-9]+){3}$"
 
     #echo "+++++++++++++++++++++++++++"
@@ -248,7 +257,7 @@ fix_version_suffices() {
        # echo "DIR_PATH = " $DIR_PATH
         FILE_NAMES=$(ls $DIR_PATH)
        # echo "$FILE_NAMES"
-        
+
        # echo ""
        # echo "should delete:"
        # echo "--------------"
@@ -256,28 +265,28 @@ fix_version_suffices() {
             File=$(echo $FILE_NAME |  grep -Pv  ${Re0}${Re1})
             echo "checking file " $File
             if [ ! -z "$File" ]  && ! [[ $File == cmake* ]] && ! [[ $File == *.a ]]
-            then 
+            then
        #         echo $File
                 rm $DIR_PATH/$File
-                
-            fi    
-        done    
-        
+
+            fi
+        done
+
         #echo ""
         #echo "should NOT delete:"
         #echo "------------------"
         for FILE_NAME in $FILE_NAMES; do
             File=$(echo $FILE_NAME |  grep -P  ${Re0}${Re1})
-            
-            if [ ! -z "$File" ] 
-            then 
+
+            if [ ! -z "$File" ]
+            then
                 NEW_NAME=$(echo $FILE_NAME | grep -Po ${Re0}"(?="${Re1}")")
             # echo $File " ->" $NEW_NAME
                 mv $DIR_PATH/$File $DIR_PATH/$NEW_NAME
                 # patchelf --set-soname $NEW_NAME $DIR_PATH/$NEW_NAME
-            fi 
-            
-        done 
+            fi
+
+        done
     done
 }
 
@@ -286,7 +295,7 @@ fix_version_suffices() {
 #----------------------------------------------------------------------------------
 
 
-USER_CONFIG_FILE=$(pwd)/user-config.jam
+USER_CONFIG_FILE=$BOOST_DIR/user-config.jam
 
 cd $BOOST_DIR
 
@@ -300,27 +309,27 @@ then
 
   ./bootstrap.sh # 2>&1 | tee -a bootstrap.log
 fi
-  
-#-------------------------------------------  
+
+#-------------------------------------------
 
 # use as many cores as available (for build)
 num_cores=$(grep -c ^processor /proc/cpuinfo)
-echo " cores available = " $num_cores 
+echo " cores available = " $num_cores
 
 
 
 
-#------------------------------------------- 
-                
+#-------------------------------------------
 
-    
-    
+
+
+
 
 
 # for LINKAGE in $LINKAGES; do
-
+    echo "Building boost for abi names: $ABI_NAMES"| tee -a ${LOG_FILE}
     for ABI_NAME in $ABI_NAMES; do
-    
+
         toolset_name="$(toolset_for_abi_name $ABI_NAME)"
         abi="$(abi_for_abi_name $ABI_NAME)"
         address_model="$(address_model_for_abi_name $ABI_NAME)"
@@ -330,20 +339,27 @@ echo " cores available = " $num_cores
         export BFA_TOOL_TRIPLE_FOR_ABI="$(tool_triple_for_abi_name $ABI_NAME)"
         export BFA_COMPILER_FLAGS_FOR_ABI="$(compiler_flags_for_abi_name $ABI_NAME)"
         export BFA_LINKER_FLAGS_FOR_ABI="$(linker_flags_for_abi_name $ABI_NAME)"
-        
+
         echo "------------------------------------------------------------"| tee -a ${LOG_FILE}
         echo "Building boost for: $ABI_NAME $LINKAGE"| tee -a ${LOG_FILE}
+        echo "\t- address model: $address_model"| tee -a ${LOG_FILE}
+        echo "\t- architecture: $arch_for_abi"| tee -a ${LOG_FILE}
+        echo "\t- variant: $BUILD_VARIANT"| tee -a ${LOG_FILE}
+        echo "\t- abi: $abi"| tee -a ${LOG_FILE}
+        echo "\t- build-dir: ${BUILD_DIR_TMP}/$ABI_NAME"| tee -a ${LOG_FILE}
+        echo "\t- lib-dir: ${LIBS_DIR}/$ABI_NAME"| tee -a ${LOG_FILE}
         echo "------------------------------------------------------------"| tee -a ${LOG_FILE}
         # toolset=clang-$toolset_name     \
-                                   
+
         {
-            ./b2 runtime-link=static binary-format=elf \
+            echo "./b2 toolset=clang runtime-link=static binary-format=elf \
                 address-model=$address_model \
                 architecture=$arch_for_abi \
                 abi=$abi    \
                 link=static                  \
                 threading=multi              \
-                target-os=android    variant=release     \
+                target-os=android    \
+                variant=$BUILD_VARIANT     \
                 --user-config=$USER_CONFIG_FILE \
                 --ignore-site-config         \
                 --layout=system           \
@@ -352,15 +368,15 @@ echo " cores available = " $num_cores
                 --build-dir=${BUILD_DIR_TMP}/$ABI_NAME \
                 --includedir=${INCLUDE_DIR} \
                 --libdir=${LIBS_DIR}/$ABI_NAME \
-                install 2>&1                 \
+                install 2>&1 "                \
                 || { echo "Error: Failed to build boost for $ABI_NAME!";}
         } | tee -a ${LOG_FILE}
-        
+
     done # for ARCH in $ARCHLIST
-    
+
 # done # for LINKAGE in $LINKAGE_LIST
 
-#------------------------------------------- 
+#-------------------------------------------
 
 
 persist_ndk_version
@@ -371,6 +387,3 @@ echo "built boost to "  ${PREFIX_DIR}
 
 
 export PATH=$SAVED_PATH
-
-
-
