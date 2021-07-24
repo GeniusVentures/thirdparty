e78a52643bd7e92c7f3d9137ba7b6db0a0801abf
devcareer00 <fldevcareer0@gmail.com>
2021-07-25 00:05:07 +0200

    Fixed iOS cross compile simplified build

--- build/iOS/Openssl-build/build-libssl.sh
+++ build/iOS/Openssl-build/build-libssl.sh
@@ -147,9 +147,9 @@ run_configure()
   echo "  Configure..."
   set +e
   if [ "${LOG_VERBOSE}" == "verbose" ]; then
-    ./Configure ${LOCAL_CONFIG_OPTIONS} no-tests | tee "${LOG}"
+    ./Configure ${LOCAL_CONFIG_OPTIONS} no-tests no-engine | tee "${LOG}"
   else
-    (./Configure ${LOCAL_CONFIG_OPTIONS} no-tests > "${LOG}" 2>&1) & spinner
+    (./Configure ${LOCAL_CONFIG_OPTIONS} no-tests no-engine > "${LOG}" 2>&1) & spinner
   fi
 
   # Check for error status
