543be7091eee20f5c80e44023cad329cba55b9fb
devcareer00 <fldevcareer0@gmail.com>
2021-09-01 14:28:53 +0200

    Added script to build openssl for Android

---
+++ build/Android/Openssl-build/build.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+#  Automatic build script for libssl and libcrypto
+#  for Android devices.
+
+# -u  Attempt to use undefined variable outputs error message, and forces an exit
+# -e  Abort script at first error, when a command exits with non-zero status (except in until or while loops, if-tests, list constructs)
+# -o pipefail  Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value
+set -ue
+
+ABI=""
+ANDROID_NATIVE_API_LEVEL=""
+BUILD_DIR=""
+LOCAL_PATH=""
+LOCAL_ANDROID_NDK=""
+LOCAL_ANDROID_TOOLCHAIN=""
+
+# Process command line arguments
+for i in "$@"
+do
+case $i in
+  --abi=*)
+    ABI="${i#*=}"
+    shift
+    ;;
+  --api-level=*)
+    ANDROID_NATIVE_API_LEVEL="${i#*=}"
+    shift
+    ;;
+  --build-dir=*)
+    BUILD_DIR="${i#*=}"
+    shift
+    ;;
+  --env_path=*)
+    LOCAL_PATH="${i#*=}"
+    shift
+    ;;
+  --env_android_ndk=*)
+    LOCAL_ANDROID_NDK="${i#*=}"
+    shift
+    ;;
+  --env_android_toolchain=*)
+    LOCAL_ANDROID_TOOLCHAIN="${i#*=}"
+    shift
+    ;;
+  *)
+    echo "Unknown argument: ${i}"
+    ;;
+esac
+done
+
+export ANDROID_NDK=$LOCAL_ANDROID_NDK
+export ANDROID_TOOLCHAIN=$LOCAL_ANDROID_TOOLCHAIN
+export PATH=$LOCAL_PATH
+export CC=clang
+OPENSSL_CONFIGURE_CMD="./Configure $ABI -D__ANDROID_API__=$ANDROID_NATIVE_API_LEVEL --prefix=$BUILD_DIR --openssldir=$BUILD_DIR"
+echo $OPENSSL_CONFIGURE_CMD
+
+$OPENSSL_CONFIGURE_CMD
+
+make
+make install
