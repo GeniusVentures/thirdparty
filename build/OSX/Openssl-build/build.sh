6d4ec7256815dff0faaf01b21fbea9dc7ef9a22c
Kenneth Hurley <ken@geniusventures.io>
2021-12-12 21:05:04 -0800

    Builds openssl universal library with allowing other systems to use the same source directory and not wait on each other

--- build/OSX/Openssl-build/build.sh
+++ build/OSX/Openssl-build/build.sh
@@ -4,6 +4,7 @@
 SRC_DIR="."
 BUILD_DIR="`pwd`/build"
 CONFIG=""
+CONFIG_DIR=""
 MACOSX_DEPLOYMENT_TARGET=10.12
 
 # Process command line arguments
@@ -32,50 +33,66 @@ case $i in
 esac
 done
 
-make clean
+INSTALL_DIR=$(cd "$BUILD_DIR/../../"; pwd)
+LIB_DIR="$INSTALL_DIR/lib"
 
-LIB_DIR=$BUILD_DIR/lib
-cd $SRC_DIR
-./Configure darwin64-x86_64-cc shared --prefix=$BUILD_DIR --openssldir=$BUILD_DIR
+echo "INSTALL_DIR is $INSTALL_DIR"
+echo "BUILD_DIR is $BUILD_DIR"
+echo "LIB_DIR is $LIB_DIR"
+
+X86_64_DIR=$BUILD_DIR/x86_64
+if [ ! -d "$X86_64_DIR" ]; then
+  mkdir -p "$X86_64_DIR"
+fi
+cd $X86_64_DIR
+if [ ! -f "Makefile" ]; then
+  $SRC_DIR/Configure darwin64-x86_64-cc shared --prefix=$INSTALL_DIR --openssldir=$BUILD_DIR
+fi
 make build_libs
-make install_dev
 
-LIB_X86_64_DIR=$BUILD_DIR/lib_x86_64
-mkdir -p $LIB_X86_64_DIR
-cp $BUILD_DIR/lib/libssl.1.1.dylib $LIB_X86_64_DIR
-cp $BUILD_DIR/lib/libssl.a $LIB_X86_64_DIR
-cp $BUILD_DIR/lib/libcrypto.1.1.dylib $LIB_X86_64_DIR
-cp $BUILD_DIR/lib/libcrypto.a $LIB_X86_64_DIR
-
-
-make clean
-
-ARM64_DIR=$BUILD_DIR/arm64
-./Configure $CONFIG darwin64-arm64-cc no-asm --prefix=$ARM64_DIR --openssldir=$ARM64_DIR
-make build_libs
-make install_dev
-
-LIB_ARM64_DIR=$BUILD_DIR/lib_arm64
-mkdir -p $LIB_ARM64_DIR
-cp $ARM64_DIR/lib/libssl.1.1.dylib $LIB_ARM64_DIR
-cp $ARM64_DIR/lib/libssl.a $LIB_ARM64_DIR
-cp $ARM64_DIR/lib/libcrypto.1.1.dylib $LIB_ARM64_DIR
-cp $ARM64_DIR/lib/libcrypto.a $LIB_ARM64_DIR
-
-lipo -create $LIB_X86_64_DIR/libssl.1.1.dylib $LIB_ARM64_DIR/libssl.1.1.dylib -output $LIB_DIR/libssl.1.1.dylib
-lipo -create $LIB_X86_64_DIR/libcrypto.1.1.dylib $LIB_ARM64_DIR/libcrypto.1.1.dylib -output $LIB_DIR/libcrypto.1.1.dylib
-lipo -create $LIB_X86_64_DIR/libssl.a $LIB_ARM64_DIR/libssl.a -output $LIB_DIR/libssl.a
-lipo -create $LIB_X86_64_DIR/libcrypto.a $LIB_ARM64_DIR/libcrypto.a -output $LIB_DIR/libcrypto.a
-if [ ! -f $LIB_DIR/libssl.dylib ]; then
-  ln -s $LIB_DIR/libssl.1.1.dylib $LIB_DIR/libssl.dylib
+if [ ! -d "$LIB_DIR/x86_64" ]; then
+  mkdir -p "$LIB_DIR/x86_64"
 fi
 
-if [ ! -f $LIB_DIR/libcrypto.dylib ]; then
-  ln -s $LIB_DIR/libcrypto.1.1.dylib $LIB_DIR/libcrypto.dylib
+cp $X86_64_DIR/lib*.dylib $LIB_DIR/x86_64
+cp $X86_64_DIR/lib*.a $LIB_DIR/x86_64
+cp $X86_64_DIR/include $INSTALL_DIR
+
+ARM64_DIR=$BUILD_DIR/arm64
+if [ ! -d "$ARM64_DIR" ]; then
+  mkdir -p $ARM64_DIR
+fi
+cd $ARM64_DIR
+if [ ! -f "Makefile" ]; then
+  $SRC_DIR/Configure $CONFIG darwin64-arm64-cc no-asm --prefix=$ARM64_DIR --openssldir=$ARM64_DIR
+fi
+
+if [ ! -d "$LIB_DIR/arm64" ]; then
+  mkdir -p "$LIB_DIR/arm64"
+fi
+
+make build_libs
+cp $ARM64_DIR/lib*.dylib $LIB_DIR/arm64
+cp $ARM64_DIR/lib*.a $LIB_DIR/arm64
+
+if [ ! -d "$LIB_DIR/universal" ]; then
+  mkdir -p $LIB_DIR/universal
+fi
+
+lipo -create $LIB_DIR/x86_64/libssl.1.1.dylib $LIB_DIR/arm64/libssl.1.1.dylib -output $LIB_DIR/universal/libssl.1.1.dylib
+lipo -create $LIB_DIR/x86_64/libcrypto.1.1.dylib $LIB_DIR/arm64/libcrypto.1.1.dylib -output $LIB_DIR/universal/libcrypto.1.1.dylib
+lipo -create $LIB_DIR/x86_64/libssl.a $LIB_DIR/arm64/libssl.a -output $LIB_DIR/universal/libssl.a
+lipo -create $LIB_DIR/x86_64/libcrypto.a $LIB_DIR/arm64/libcrypto.a -output $LIB_DIR/universal/libcrypto.a
+if [ ! -f $LIB_DIR/universal/libssl.dylib ]; then
+  ln -s $LIB_DIR/universal/libssl.1.1.dylib $LIB_DIR/universal/libssl.dylib
+fi
+
+if [ ! -f $LIB_DIR/universal/libcrypto.dylib ]; then
+  ln -s $LIB_DIR/universal/libcrypto.1.1.dylib $LIB_DIR/universal/libcrypto.dylib
 fi
 
 # print fat library info
-lipo -info $LIB_DIR/libssl.1.1.dylib
-lipo -info $LIB_DIR/libssl.dylib
-lipo -info $LIB_DIR/libcrypto.1.1.dylib
-lipo -info $LIB_DIR/libcrypto.dylib
+lipo -info $LIB_DIR/universal/libssl.1.1.dylib
+lipo -info $LIB_DIR/universal/libssl.dylib
+lipo -info $LIB_DIR/universal/libcrypto.1.1.dylib
+lipo -info $LIB_DIR/universal/libcrypto.dylib
