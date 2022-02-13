008c1f84ef7dc1f58cacd805adde5344670e2015
Kenneth Hurley <ken@geniusventures.io>
2022-02-12 19:15:16 -0800

    Override kompute build to just build the library (.a) file for kompute, linking can now happen in SDK with kompute, MoltenVK

---
+++ build/cmake/kompute-fix.cmake
@@ -0,0 +1,19 @@
+
+
+get_filename_component(THIRD_PARTY_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../../" DIRECTORY ABSOLUTE)
+include_directories(${THIRD_PARTY_DIRECTORY}/Vulkan-Headers/include)
+
+# Make find_package() a no-op if argument is in the list of subprojects.
+#macro(find_package)
+#   if(NOT "${ARGV0}" STREQUAL "Vulkan")
+#        _find_package(${ARGV})
+#    endif()
+#endmacro()
+#
+#macro(target_link_libraries)
+#    if(NOT "${ARGV1}" STREQUAL "Vulkan::Vulkan")
+#        _target_link_libraries(${ARGV})
+#    else()
+#        _target_link_libraries(kompute)
+#    endif()
+#endmacro()
