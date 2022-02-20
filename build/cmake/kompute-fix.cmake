5490820e31a05d446b89429dae9211ce26286a40
devcareer00 <fldevcareer0@gmail.com>
2022-02-20 20:01:25 +0100

    Fixing cmake parsing error

--- build/cmake/kompute-fix.cmake
+++ build/cmake/kompute-fix.cmake
@@ -2,12 +2,12 @@
 
 get_filename_component(THIRD_PARTY_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../../" DIRECTORY ABSOLUTE)
 
-set(_VULKAN_HEADERS_INCLUDE ${THIRD_PARTY_DIRECTORY}/Vulkan-Headers/include)
+set(_VULKAN_HEADERS_INCLUDE "${THIRD_PARTY_DIRECTORY}/Vulkan-Headers/include")
 if(ANDROID)
   # Use Android NDK builtin vulkan header
-  set(_VULKAN_HEADERS_INCLUDE ${CMAKE_SYSROOT}/usr/include/vulkan)
+  set(_VULKAN_HEADERS_INCLUDE "${CMAKE_SYSROOT}/usr/include/vulkan")
 endif
-include_directories(${_VULKAN_HEADERS_INCLUDE})
+include_directories("${_VULKAN_HEADERS_INCLUDE}")
 
 # Make find_package() a no-op if argument is in the list of subprojects.
 macro(find_package)
