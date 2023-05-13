725915d2c1825782d6f0e3127300b5faedcfd282
Super Genius <ken+github@gnus.ai>
2023-05-12 18:42:37 -0700

    Moving to patch_command as configure_command expects some sort of makefile to be generated

--- build/config-wallet-core.sh
+++ build/config-wallet-core.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 
-pushd $1
+pushd $1 >/dev/null
 mkdir -p build/local/bin
 mkdir -p build/local/include
 mkdir -p build/local/lib
 if [ ! -f src/Generated/CoinInfoData.cpp ]; then
   tools/generate-files
 fi
-popd
+
+popd >/dev/null
+
