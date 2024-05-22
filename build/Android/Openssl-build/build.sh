8fa2bb115b0974aef1c540a941687c4279f95d3a
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-05-22 14:23:18 -0300

    chore: Fixed OpenSSL build

--- build/Android/Openssl-build/build.sh
+++ build/Android/Openssl-build/build.sh
@@ -14,64 +14,61 @@ BUILD_DIR=""
 LOCAL_PATH=""
 LOCAL_ANDROID_NDK=""
 LOCAL_ANDROID_TOOLCHAIN=""
-DEBUG_BUILD=""
+BUILD_TYPE="--release"
 
 # Process command line arguments
-for i in "$@"
-do
-case $i in
-  --abi=*)
-    ABI="${i#*=}"
-    shift
-    ;;
-  --api-level=*)
-    ANDROID_NATIVE_API_LEVEL="${i#*=}"
-    shift
-    ;;
-  --build-dir=*)
-    BUILD_DIR="${i#*=}"
-    shift
-    ;;
-  --env_path=*)
-    LOCAL_PATH="${i#*=}"
-    shift
-    ;;
-  --env_android_ndk=*)
-    LOCAL_ANDROID_NDK="${i#*=}"
-    shift
-    ;;
-  --env_android_toolchain=*)
-    LOCAL_ANDROID_TOOLCHAIN="${i#*=}"
-    shift
-    ;;
-  --debug)
-    DEBUG_BUILD="--debug"
-    shift
-    ;;
-  *)
-    echo "Unknown argument: ${i}"
-    ;;
-esac
+for i in "$@"; do
+    case $i in
+    --abi=*)
+        ABI="${i#*=}"
+        shift
+        ;;
+    --api-level=*)
+        ANDROID_NATIVE_API_LEVEL="${i#*=}"
+        shift
+        ;;
+    --build-dir=*)
+        BUILD_DIR="${i#*=}"
+        shift
+        ;;
+    --env_path=*)
+        LOCAL_PATH="${i#*=}"
+        shift
+        ;;
+    --env_android_ndk=*)
+        LOCAL_ANDROID_NDK="${i#*=}"
+        shift
+        ;;
+    --env_android_toolchain=*)
+        LOCAL_ANDROID_TOOLCHAIN="${i#*=}"
+        shift
+        ;;
+    --debug)
+        BUILD_TYPE="--debug"
+        shift
+        ;;
+    *)
+        echo "Unknown argument: ${i}"
+        ;;
+    esac
 done
 
-export ANDROID_NDK=$LOCAL_ANDROID_NDK
+export ANDROID_NDK_ROOT=$LOCAL_ANDROID_NDK
 export ANDROID_TOOLCHAIN=$LOCAL_ANDROID_TOOLCHAIN
 export PATH=$LOCAL_ANDROID_NDK:$LOCAL_ANDROID_TOOLCHAIN:$LOCAL_PATH
-export CC=clang
-CONFIGDIR=`dirname $0`/../../../openssl
-REALCONFIGDIR=`realpath ${CONFIGDIR}`
 
-if [ -n "$DEBUG_BUILD" ]; then
-  OPENSSL_CONFIGURE_CMD="${REALCONFIGDIR}/Configure $DEBUG_BUILD no-asm no-shared $ABI --prefix=$BUILD_DIR --openssldir=$BUILD_DIR"
-else
-  OPENSSL_CONFIGURE_CMD="${REALCONFIGDIR}/Configure no-asm no-shared $ABI --prefix=$BUILD_DIR --openssldir=$BUILD_DIR"
-fi
+CONFIGDIR=$(dirname $0)/../../../openssl
+REALCONFIGDIR=$(realpath ${CONFIGDIR})
+
+OPENSSL_CONFIGURE_CMD="${REALCONFIGDIR}/Configure $ABI $BUILD_TYPE no-asm no-shared --prefix=$BUILD_DIR --openssldir=$BUILD_DIR"
 
 echo "Building OpenSSL"
 echo $OPENSSL_CONFIGURE_CMD
+echo
 
-echo "Environment variables: \nPATH="$PATH
-which clang
+echo "Environment variables"
+echo $PATH
+echo
 
 $OPENSSL_CONFIGURE_CMD
 
