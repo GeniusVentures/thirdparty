076e76fdf3d47d5fc98dd58d0d2d4982f33d8feb
Super Genius <ken+git@gnus.ai>
2024-07-18 14:34:25 -0700

    updating openssl to build fat libraries (very fast) on OSX

--- build/OSX/Openssl-build/build.sh
+++ build/OSX/Openssl-build/build.sh
@@ -26,6 +26,10 @@ case $i in
     DEBUG_FLAGS="-g"
     shift
     ;;
+  --release)
+    DEBUG_FLAGS=""
+    shift
+    ;;
   *)
     echo "Unknown argument: ${i}"
     ;;
@@ -35,60 +39,36 @@ done
 INSTALL_DIR=$BUILD_DIR
 LIB_DIR="$INSTALL_DIR/lib"
 
-X86_64_DIR=$BUILD_DIR/x86_64
+X86_64_DIR="$LIB_DIR/x86_64"
 if [ ! -d "$X86_64_DIR" ]; then
   mkdir -p "$X86_64_DIR"
 fi
 cd $X86_64_DIR
 if [ ! -f "Makefile" ]; then
-  $SRC_DIR/Configure $DEBUG_FLAGS darwin64-x86_64-cc no-asm shared --prefix=$INSTALL_DIR --openssldir=$INSTALL_DIR
+  $SRC_DIR/Configure $DEBUG_FLAGS darwin64-x86_64-cc -static --prefix=$LIB_DIR --openssldir=$X86_64_DIR
 fi
-make build_libs
+make -j`nproc` build_generated libcrypto.a libssl.a libcrypto.pc libssl.pc openssl.pc
 
-if [ ! -d "$LIB_DIR/x86_64" ]; then
-  mkdir -p "$LIB_DIR/x86_64"
-fi
-
-if [ ! -d "$LIB_DIR/pkgconfig" ]; then
-  mkdir -p "$LIB_DIR/pkgconfig"
+if [ ! -d "$LIB_DIR/pkgconfig/" ]; then
+  mkdir -p "$LIB_DIR/pkgconfig/"
 fi
 
-cp $X86_64_DIR/lib*.dylib $LIB_DIR/x86_64
-cp $X86_64_DIR/lib*.a $LIB_DIR/x86_64
+#cp $X86_64_DIR/lib*.dylib $LIB_DIR/x86_64
+#cp $X86_64_DIR/lib*.a $LIB_DIR/x86_64
 cp -R $X86_64_DIR/include $INSTALL_DIR/
 cp -R $SRC_DIR/include/openssl $INSTALL_DIR/include/
 cp -R $X86_64_DIR/*.pc $LIB_DIR/pkgconfig
 
-ARM64_DIR=$BUILD_DIR/arm64
+ARM64_DIR=$LIB_DIR/arm64
 if [ ! -d "$ARM64_DIR" ]; then
   mkdir -p $ARM64_DIR
 fi
 cd $ARM64_DIR
 if [ ! -f "Makefile" ]; then
-  $SRC_DIR/Configure $DEBUG_FLAGS darwin64-arm64-cc no-asm --prefix=$ARM64_DIR --openssldir=$ARM64_DIR
+  $SRC_DIR/Configure $DEBUG_FLAGS darwin64-arm64-cc no-asm -static --prefix=$LIB_DIR --openssldir=$ARM64_DIR
 fi
 
-if [ ! -d "$LIB_DIR/arm64" ]; then
-  mkdir -p "$LIB_DIR/arm64"
-fi
+make -j`nproc` build_generated libcrypto.a libssl.a
 
-make build_libs
-cp $ARM64_DIR/lib*.dylib $LIB_DIR/arm64
-cp $ARM64_DIR/lib*.a $LIB_DIR/arm64
-
-
-lipo -create $LIB_DIR/x86_64/libssl.1.1.dylib $LIB_DIR/arm64/libssl.1.1.dylib -output $LIB_DIR/libssl.1.1.dylib
-lipo -create $LIB_DIR/x86_64/libcrypto.1.1.dylib $LIB_DIR/arm64/libcrypto.1.1.dylib -output $LIB_DIR/libcrypto.1.1.dylib
-lipo -create $LIB_DIR/x86_64/libssl.a $LIB_DIR/arm64/libssl.a -output $LIB_DIR/libssl.a
-lipo -create $LIB_DIR/x86_64/libcrypto.a $LIB_DIR/arm64/libcrypto.a -output $LIB_DIR/libcrypto.a
-if [ ! -f $LIB_DIR/libssl.dylib ]; then
-  ln -s $LIB_DIR/libssl.1.1.dylib $LIB_DIR/libssl.dylib
-fi
-
-if [ ! -f $LIB_DIR/libcrypto.dylib ]; then
-  ln -s $LIB_DIR/libcrypto.1.1.dylib $LIB_DIR/libcrypto.dylib
-fi
-
-# print fat library info
-lipo -info $LIB_DIR/libssl.1.1.dylib
-lipo -info $LIB_DIR/libcrypto.1.1.dylib
+lipo -create $X86_64_DIR/libssl.a $ARM64_DIR/libssl.a -output $LIB_DIR/libssl.a
+lipo -create $X86_64_DIR/libcrypto.a $ARM64_DIR/libcrypto.a -output $LIB_DIR/libcrypto.a
