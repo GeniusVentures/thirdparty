61389cfdc8fa065d787216d45e83d1c34ea90c65
Henrique A Klein <henryaklein@gmail.com>
2024-08-07 08:20:55 -0700

    Fix: Install of OpenSSL on the build script

--- build/OSX/Openssl-build/build.sh
+++ build/OSX/Openssl-build/build.sh
@@ -51,52 +51,42 @@ fi
 
 INSTALL_DIR=$BUILD_DIR
 LIB_DIR="$INSTALL_DIR/lib"
-OUT_DIR=""
 
 if $X86_64_PRESENT; then
-  X86_64_DIR="$LIB_DIR/x86_64"
-  if [ ! -d "$X86_64_DIR" ]; then
-    mkdir -p "$X86_64_DIR"
-  fi
-  cd $X86_64_DIR
   if [ ! -f "Makefile" ]; then
-    $SRC_DIR/Configure $DEBUG_FLAGS darwin64-x86_64-cc -static --prefix=$LIB_DIR --openssldir=$X86_64_DIR -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET
+    $SRC_DIR/Configure $DEBUG_FLAGS darwin64-x86_64-cc -static --prefix=$BUILD_DIR --openssldir=$BUILD_DIR -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET
   fi
   make build_generated libcrypto.a libssl.a
-  OUT_DIR=$X86_64_DIR
+  make install_dev
 fi
 
 if $ARM64_PRESENT; then
-  ARM64_DIR=$LIB_DIR/arm64
-  if [ ! -d "$ARM64_DIR" ]; then
-    mkdir -p $ARM64_DIR
+
+  ARM64_DIR="$BUILD_DIR"
+
+  if $X86_64_PRESENT; then
+    ARM64_DIR="$LIB_DIR/arm64"
+    mkdir -p "$ARM64_DIR"
+    cd $ARM64_DIR
   fi
-  cd $ARM64_DIR
-  if [ ! -f "Makefile" ]; then
-    $SRC_DIR/Configure $DEBUG_FLAGS darwin64-arm64-cc no-asm -static --prefix=$LIB_DIR --openssldir=$ARM64_DIR -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET
-  fi
+
+  $SRC_DIR/Configure $DEBUG_FLAGS darwin64-arm64-cc no-asm -static --prefix=$ARM64_DIR --openssldir=$ARM64_DIR -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET
 
   make build_generated libcrypto.a libssl.a
-  OUT_DIR=$ARM64_DIR
+  if ! $X86_64_PRESENT ; then
+      make install_dev
+  fi
 fi
-
-make build_generated libcrypto.pc libssl.pc openssl.pc
-if [ ! -d "$LIB_DIR/pkgconfig/" ]; then
-  mkdir -p "$LIB_DIR/pkgconfig/"
-fi
-
-cp -R $OUT_DIR/include $INSTALL_DIR/
-cp -R $SRC_DIR/include/openssl $INSTALL_DIR/include/
-cp -R $OUT_DIR/*.pc $LIB_DIR/pkgconfig
 
 
 if $X86_64_PRESENT && $ARM64_PRESENT; then
   echo "Building the fat library"
-  lipo -create $X86_64_DIR/libssl.a $ARM64_DIR/libssl.a -output $LIB_DIR/libssl.a
-  lipo -create $X86_64_DIR/libcrypto.a $ARM64_DIR/libcrypto.a -output $LIB_DIR/libcrypto.a
-  rm -rf $ARM64_DIR/
-  rm -rf $X86_64_DIR/
-else
-  mv $OUT_DIR/libssl.a $LIB_DIR/
-  mv $OUT_DIR/libcrypto.a $LIB_DIR/
+  mv $LIB_DIR/libssl.a $LIB_DIR/libssltemp.a 
+  mv $LIB_DIR/libcrypto.a $LIB_DIR/libcryptotemp.a 
+
+  lipo -create $ARM64_DIR/libssl.a $LIB_DIR/libssltemp.a -output $LIB_DIR/libssl.a
+  lipo -create $ARM64_DIR/libcrypto.a $LIB_DIR/libssltemp.a -output $LIB_DIR/libcrypto.a
+  rm -rf $ARM64_DIR
+  rm $LIB_DIR/libssltemp.a 
+  rm $LIB_DIR/libcryptotemp.a 
 fi
