58d19c04cfd5a430d16e3545dd8713ea4bd47999
itsafuu <sariru@gmail.com>
2025-01-18 21:00:23 -0500

    Combine the correct libraries together

--- build/OSX/Openssl-build/build.sh
+++ build/OSX/Openssl-build/build.sh
@@ -54,7 +54,7 @@ LIB_DIR="$INSTALL_DIR/lib"
 
 if $X86_64_PRESENT; then
   if [ ! -f "Makefile" ]; then
-    $SRC_DIR/Configure $DEBUG_FLAGS darwin64-x86_64-cc -static --prefix=$BUILD_DIR --openssldir=$BUILD_DIR -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET
+    $SRC_DIR/Configure $DEBUG_FLAGS darwin64-x86_64-cc no-asm -static --prefix=$BUILD_DIR --openssldir=$BUILD_DIR -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET
   fi
   make build_generated libcrypto.a libssl.a
   make install_dev
@@ -85,7 +85,7 @@ if $X86_64_PRESENT && $ARM64_PRESENT; then
   mv $LIB_DIR/libcrypto.a $LIB_DIR/libcryptotemp.a 
 
   lipo -create $ARM64_DIR/libssl.a $LIB_DIR/libssltemp.a -output $LIB_DIR/libssl.a
-  lipo -create $ARM64_DIR/libcrypto.a $LIB_DIR/libssltemp.a -output $LIB_DIR/libcrypto.a
+  lipo -create $ARM64_DIR/libcrypto.a $LIB_DIR/libcryptotemp.a -output $LIB_DIR/libcrypto.a
   rm -rf $ARM64_DIR
   rm $LIB_DIR/libssltemp.a 
   rm $LIB_DIR/libcryptotemp.a 
